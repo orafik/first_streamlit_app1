a9a1fc76d5d86b242a7cd8e466cba37391970cc0
orafik <123893171+orafik@users.noreply.github.com>
2023-02-08 13:53:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,4 @@ streamlit.txt(fruity_response.json())
 
 
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
