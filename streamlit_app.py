51e8db529decf6cf46b1182efc62d8bc1d9897bf
orafik <123893171+orafik@users.noreply.github.com>
2023-02-08 13:27:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,9 @@ streamlit.dataframe(my_fruit_list)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"watermelon")
+streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
