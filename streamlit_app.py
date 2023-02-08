abf542df677d37998cc352671a4d13cc7ecdc28f
orafik <123893171+orafik@users.noreply.github.com>
2023-02-08 13:05:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,7 @@ streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
 
