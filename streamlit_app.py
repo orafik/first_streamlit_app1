794fcb39149d6cdcab2bcf2b4afeb5270e00ddb2
orafik <123893171+orafik@users.noreply.github.com>
2023-02-08 13:16:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
+
 streamlit.title ('My Mom\'s New Healthy Diner')
+
 streamlit.header ('Breakfast Favorites')
+
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
@@ -15,10 +18,13 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
 
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
+
 
 
 
