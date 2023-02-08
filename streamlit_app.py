38d4d55e8f0241b1cf17afaa3644b117b8be9751
orafik <123893171+orafik@users.noreply.github.com>
2023-02-07 23:26:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Acpcado', 'Strawberry'])
+
+streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado', 'Strawberry'])
