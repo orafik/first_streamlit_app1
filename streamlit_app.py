f42a8eae5abc01373cf94802d825f40618bcaa8a
orafik <123893171+orafik@users.noreply.github.com>
2023-02-09 13:51:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 try:
   fruit_choice = streamlit.text_input ("What fruit would you like information about")
-   if not fruit_choice:
+if not fruit_choice:
       streamlit.error ("Please select a fruit to get information")
       else:
         fruityvice_response = requests.get ("Https://fruityvice.com/api/fruit/" + fruit_choice
