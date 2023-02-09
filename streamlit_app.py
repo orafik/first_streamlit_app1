257eceae2a8594bfb09281251565032acf22b092
orafik <123893171+orafik@users.noreply.github.com>
2023-02-09 15:47:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,14 +51,17 @@ try:
 except URLError as e:
   
     streamlit.stop()
-    
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_cur = my_cnx.cursor()
-    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-    my_data_rows = my_cur.fetchall()
     streamlit.header("The Fruit Load List Contains:")
-        
-streamlit.dataframe(my_data_rows)
+    def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+          return my_cur.fetchall()
+      
+      if streamlit.button ('Get FRuit Load List')
+           my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+           my_data_rows = get_fruit_load_lidt()
+           streamlit.dataframe(my_data_rows)
+            
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
