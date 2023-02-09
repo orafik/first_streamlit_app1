bc493c10ada9193c143187292972cd5619727ac7
orafik <123893171+orafik@users.noreply.github.com>
2023-02-09 14:21:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,8 @@ try:
        streamlit.dataframe (fruityvice_normalized)
       
   except URLError as e:
-        
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         my_cur = my_cnx.cursor()
         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
         my_data_rows = my_cur.fetchall()
