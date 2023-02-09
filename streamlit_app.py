71937b6e2631edf1908d3f77f4d7efa12bf361a9
orafik <123893171+orafik@users.noreply.github.com>
2023-02-09 13:50:26 -0500

    Update streamlit_app.py
    try:
    
      fruit_choice = streamlit.text_input('What fruit would you like information about?')
      
      if not fruit_choice:
          
             streamlit.error ("Please select a fruit to get information")
            
      else:
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
             fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
             streamlit.dataframe (fruityvice_normalized)
              
      except URLError as e:
        streamlit.error()

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,24 +32,21 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+
 try:
-
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  
-  if not fruit_choice:
-      
-         streamlit.error ("Please select a fruit to get information")
-        
-  else:
-         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-         streamlit.dataframe (fruityvice_normalized)
+  fruit_choice = streamlit..text_input ("What fruit would you like information about")
+   if not fruit_choice:
+      streamlit.error ("Please select a fruit to get information")
+      else:
+        fruityvice_response = requests.get ("Https://fruityvice.com/api/fruit/" + fruit_choice
+        fruityvice.normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe (fruityvice _ normalized)
+      except URLError as e:
+               
           
-  except URLError as e:
-    streamlit.error()
-
-
-
+  
+  
+  
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
