c53ee51d9f834fb34ceb13cd5ce5f3e543be2b51
orafik <123893171+orafik@users.noreply.github.com>
2023-02-09 13:33:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
 try:
 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -44,8 +45,7 @@ try:
          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
          streamlit.dataframe(fruityvice_normalized)
 
-  except: URLError as e:
-    streamlit.error()
+ 
 
 
 
