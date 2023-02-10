a0c2a68b6297c19c04058034e36b9cc90bef4ee5
orafik <123893171+orafik@users.noreply.github.com>
2023-02-09 21:32:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
                       
+                      
 
 
 
